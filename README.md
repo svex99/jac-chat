2587637493f3d89e72b57a45e5a077736c3f1d34
Enmanuel Verdesia Suárez <everdesiasuarez@gamil.com>
2021-04-22 22:02:36 -0400

    MT server is pretty well

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # jac-chat
 Chat using zmq sockets with an architecture client-server, but this time the server implement a multithread philosophy and the pattern dispatcher-worker.
+
+## Implementación
+
+La implementación de `jac-chat` parte del chat de grupo `0mq-chat` (cliente-servidor) implementado sobre sockets ZMQ en una tarea anterior. `jac-chat` mantiene las funcionalidades de este:
+
+- La comunicación entre cada cliente y el servidor se asíncrona.
+- Cada mensaje que llega de un cliente al servidor es enviado a los demás clientes.
+- Los clientes son notificados cuando otro entra/sale del chat. 
+- Los clientes son notificados cuando el servidor se cierra (`ctrl+C`).
+- Si el servidor se reinicia los clientes siguen mateniendo la conexión.
+  
+Pero a diferencia del servidor de `0mq-chat`, el cual desde un mismo hilo, recibía un request, la enviaba a todos los clientes, y una vez que esto se completaba era que podía procesar el siguiente request, `jac-chat` sigue el patrón **Dispacther-Worker** con una filosofía multihilo en el servidor. Un hilo (Dispatcher) recibe los request, los coloca en una cola y otro hilo que este libre, de un conjunto de hilos (Workers), procesa el request enviándolo a todos los clientes que sea necesario. De esta forma el server procesa los requests de forma paralela en cada uno de los workers y ofrece un mejor tiempo de respuesta ante la latencia de la red.
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 
 import zmq
 
@@ -12,31 +13,57 @@ session_file = 'server_session.json'
 
 class Server:
 
-    def __init__(self, name, port, ip):
+    def __init__(self, name, interface, port, num_workers):
         self.name = name
-        self.addr = (ip, port)
-        self.socket = zmq.Context().socket(zmq.ROUTER)
+        self.interface = interface
+        self.port = port
+
+        self.num_workers = num_workers
+        self.stop_workers = threading.Event()   # pill for kill worker's thread
+        self.workers_pool = []
+
+        self.context = zmq.Context()
+        self.clients_socket = self.context.socket(zmq.ROUTER)
+        self.workers_socket = self.context.socket(zmq.DEALER)
+
+        self.clients_lock = threading.Lock()   # lock for sync changes in the list of clients
         self.clients = []
 
-    def start(self):
-        self.socket.bind(f'tcp://*:{self.addr[1]}')
-        logging.info(f'[{self.name}] binded to {self.addr}')
+    def worker(self):
+        """
+        Worker that processes pending requests in queue.
+        1. Gets the request from the queue.
+        2. Sends the request to all clients (except the original sender).
+        """
+        thread_name = threading.current_thread().name
+        socket = self.context.socket(zmq.DEALER)
+        socket.connect('inproc://workers')
 
-        while True:
-            client_id = self.socket.recv()
+        # handle requests while main thread keeps running
+        while not self.stop_workers.is_set():
+            try:
+                event = socket.poll(timeout=1000)
+            except zmq.error.ContextTerminated:
+                break
+            else:
+                if event == 0:
+                    continue
 
-            print('-' * 30)
+            client_id = socket.recv()
 
-            data = {k: v for k, v in self.socket.recv_json().items()}
-            logging.info(f'Data received from [{client_id}] {data}')
+            data = {k: v for k, v in socket.recv_json().items()}
+            logging.info(f'[{thread_name}] data received from [{client_id}] : {data}')
 
             is_auth = data.get('auth', None)
             joined = None
+
+            self.clients_lock.acquire()
+
             if is_auth == 1 or (is_auth is None and client_id not in self.clients):
                 self.clients.append(client_id)
                 joined = True
-                self.socket.send(client_id, zmq.SNDMORE)
-                self.socket.send_json(
+                socket.send(client_id, zmq.SNDMORE)
+                socket.send_json(
                     {
                         'name': self.name,
                         'text': f'You are now online. {len(self.clients)} online.\n'
@@ -47,8 +74,10 @@ class Server:
                     self.clients.remove(client_id)
                     joined = False
                 except ValueError:
-                    logging.warning('Disconnected client of previous session.')
+                    logging.warning(f'[{thread_name}] disconnected client of previous session.')
                     pass
+
+            self.clients_lock.release()
 
             if joined is not None:
                 msg = f'{data["name"]} {"joined" if joined else "left"} the room. ' \
@@ -60,8 +89,8 @@ class Server:
                 }
                 for client in self.clients:
                     if client != client_id:
-                        self.socket.send(client, zmq.SNDMORE)
-                        self.socket.send_json(join_data)
+                        socket.send(client, zmq.SNDMORE)
+                        socket.send_json(join_data)
 
                 logging.info(msg)
 
@@ -69,41 +98,76 @@ class Server:
                 count = 0
                 for client in self.clients:
                     if client != client_id:
-                        self.socket.send(client, zmq.SNDMORE)
-                        self.socket.send_json(data)
+                        socket.send(client, zmq.SNDMORE)
+                        socket.send_json(data)
                         count += 1
 
-                logging.info(f'Delivered data from [{client_id}] to {count} clients.')
+                logging.info(f'[{thread_name}] delivered data from [{client_id}] to {count} clients.')
+
+        socket.close(linger=1)
+        logging.info(f'Stopped {thread_name}')
+
+    def start(self):
+        clients_conn = f'tcp://{self.interface}:{self.port}'
+        self.clients_socket.bind(clients_conn)
+        logging.info(f'[{self.name}] binded to {clients_conn}')
+
+        workers_conn = 'inproc://workers'
+        self.workers_socket.bind(workers_conn)
+
+        # launch pool of working threads
+        for i in range(self.num_workers):
+            thread = threading.Thread(target=self.worker, name=f'worker{i}')
+            self.workers_pool.append(thread)
+            thread.start()
+            logging.info(f'Started worker{i}')
+
+        zmq.device(zmq.QUEUE, self.clients_socket, self.workers_socket)
 
     def disconnect(self):
+        # notify clients that server is going off
         for client in self.clients:
-            self.socket.send(client, zmq.SNDMORE)
-            self.socket.send_json({
+            self.clients_socket.send(client, zmq.SNDMORE)
+            self.clients_socket.send_json({
                 "name": self.name,
                 "text": 'Server is off.\n'
             })
-        self.socket.close(linger=1)
 
+        # close clients socket
+        self.clients_socket.close(linger=1)
+        logging.info('Closed clients socket')
+
+        # close workers socket
+        self.workers_socket.close(linger=1)
+        logging.info('Closed workers socket')
+
+        # stop workers
+        logging.info('Stopping workers...')
+        self.stop_workers.set()
+
+        # end context
+        self.context.term()
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('--port', type=int, help='Server port')
-    parser.add_argument('--ip', default='*', help='Server IP. Use \'all\' for *')
     parser.add_argument('--name', type=str, default='server', help='Server name')
+    parser.add_argument('--inter', default='*', help='Server interface to bind. Use \'all\' for *')
+    parser.add_argument('--port', type=int, help='Server port')
+    parser.add_argument('--numw', type=int, default=3, help='Number of worker threads')
 
     args = parser.parse_args()
-    args.ip = '*' if args.ip == 'all' else args.ip
+    args.inter = '*' if args.inter == 'all' else args.inter
 
     server = Server(
         name=args.name,
+        interface=args.inter,
         port=args.port,
-        ip=args.ip
+        num_workers=args.numw
     )
 
     try:
         server.start()
     except KeyboardInterrupt:
         server.disconnect()
-        logging.info('Server stopped by user!')
